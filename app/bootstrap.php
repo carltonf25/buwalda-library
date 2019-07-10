eb78c1dbd7714b03cdc8f1a2644aa23192474b78
carltonf25 <carltonfreeman24@gmail.com>
2019-07-10 12:46:33 -0400

    Remove MailGun implementation and use Mandrill for Mailer class instead

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -8,6 +8,7 @@ require_once 'helpers/url_helper.php';
 require_once 'helpers/session_helper.php';
 
 // Autoload Core Libraries
+require '../vendor/autoload.php';
 spl_autoload_register(function($className){
     require_once 'libraries/' . $className . '.php';
 });
--- app/controllers/Mailers.php
+++ app/controllers/Mailers.php
@@ -10,47 +10,61 @@ class Mailers extends Controller
     $this->apiKey = API_KEY;
     $this->domain = "sandbox0b65a44345d140a690d3d9f0ea1d720b.mailgun.org";
     $this->baseUrl = "https://api:$this->apiKey@api.mailgun.net/v3/$this->domain";
-
-    # Instantiate the client.
-    // $this->mgClient = new Mailgun($this->apiKey);
+    $this->client = new Mandrill($this->apiKey);
   }
 
   public function sendBorrowNotification($user, $book)
   {
-    $url = "$this->baseUrl/messages";
-    $fields_string = "";
-    $fields = array(
-     "from" => urlencode("Buwalda Library <library@$this->domain>"),
-     "to" => urlencode("carltonfreeman24@gmail.com"),
-     "subject" => urlencode("$user would like to borrow a book"),
-     "text" => urlencode("$user would like to borrow your book, $book. Log in to library app to view the request.")
-    );
 
-    foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }    
-    $fields_string = rtrim($fields_string, '&');
+    try {
+      // Mandrill client implementation
+      $message = array(
+        'html' => '<h1>Buwalda Library</h1>
+                 <h2>Someone would like to borrow a book</h2>
+                 <p>' . $user . ' would like to borrow your book, ' . $book . '.</p>
+                 <p>- Buwalda Library</p>
+      ',
+        'text' => $user . ' would like to borrow your book, ' . $book . '.',
+        'subject' => $user . 'requested to borrow a book.',
+        'from_email' => 'hello@buwaldalibrary.com',
+        'from_name' => 'Library',
+        'to' => array(
+          array(
+            'email' => 'carltonfreeman24@gmail.com',
+            'name' => 'Carlton',
+            'type' => 'to'
+          )
+        ),
+        'headers' => array('Reply-To' => 'hello@buwaldalibrary.com'),
+        'important' => false,
+        'track_opens' => null,
+        'track_clicks' => null,
+        'auto_text' => null,
+        'auto_html' => null,
+        'inline_css' => null,
+        'url_strip_qs' => null,
+        'preserve_recipients' => null,
+        'view_content_link' => null,
+        'tracking_domain' => null,
+        'signing_domain' => null,
+        'return_path_domain' => null,
+        'merge' => true,
+        'merge_language' => 'mailchimp'
+      );
+      $async = false;
+      $ip_pool = 'Main Pool';
+      $result = $this->client->messages->send($message, $async, $ip_pool);
 
-    // create curl resource
-    $ch = curl_init();
-
-    //set the url, number of POST vars, POST data
-    curl_setopt($ch,CURLOPT_URL, $url);
-    curl_setopt($ch,CURLOPT_POST, count($fields));
-    curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
-
-    //return the transfer as a string
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-    // $output contains the output string
-    $result = curl_exec($ch);
-
-    if ($result) {
-      return true;
-    } else {
-      return false;
+      if ($result) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (Mandrill_Error $e) {
+      // Mandrill errors are thrown as exceptions
+      echo 'A mandrill error occurred: ' . get_class($e) . ' - ' . $e->getMessage();
+      // A mandrill error occurred: Mandrill_Unknown_Subaccount - No subaccount exists with the id 'customer-123'
+      throw $e;
     }
-
-    // close curl resource to free up system resources
-    curl_close($ch);  
   }
-
 }
