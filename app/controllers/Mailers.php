282b785f406f585fb6f69df003027f0182ca758a
carltonf25 <carltonfreeman24@gmail.com>
2019-07-11 13:30:46 -0400

    Property cleanup in Mailer class

--- app/controllers/Mailers.php
+++ app/controllers/Mailers.php
@@ -59,8 +59,7 @@ class Mailers extends Controller
       }
     } catch (Mandrill_Error $e) {
       // Mandrill errors are thrown as exceptions
-      echo 'A mandrill error occurred: ' . get_class($e) . ' - ' . $e->getMessage();
-      // A mandrill error occurred: Mandrill_Unknown_Subaccount - No subaccount exists with the id 'customer-123'
+      echo 'Error sending notification: ' . get_class($e) . ' - ' . $e->getMessage();
       throw $e;
     }
   }
